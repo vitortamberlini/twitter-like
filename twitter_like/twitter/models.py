2ca5e001489edfc27444ee5a9518e151bdf47afe
Vitor Tamberlini <vitor.tamberlini@vortal.biz>
2021-11-01 11:41:11 +0000

    Created the models 'User' and 'Post'

--- twitter_like/twitter/models.py
+++ twitter_like/twitter/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=60)
+    pub_date = models.DateTimeField(default=timezone.now)
+    _author = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return f'{self.title} - {self.author}'
+
+    @property
+    def author(self):
+        if not self._author:
+            return 'User Deleted'
+        return self._author
+
